b8049feb9474d889343293173f8afc02edb3ab22
Priya Raut <rautpriya215@gmail.com>
2020-06-13 15:34:36 +0530

    Update SelectionSort.java
    Use for loop instead of while for a cleaner code with less local variables.

--- src/com/sortingalgos/SelectionSort.java
+++ src/com/sortingalgos/SelectionSort.java
@@ -6,21 +6,18 @@ public class SelectionSort {
     public static int[] sortWithSelectionSortAlgo(int[] arr){
         System.out.println(" ### Selection Sort ### ");
         System.out.println("Before sorting: " + Arrays.toString(arr));
-        int startIndex = 0, minIndex = 0, len = arr.length, i = 1, temp = 0;
-        while(startIndex < len){
-            while(i < len){
-                if(arr[i] < arr[minIndex]) {
-                    minIndex = i;
+        int minIndex, len = arr.length, temp;
+        for(int i = 0; i < len; i++){
+            minIndex = i;
+            for(int j = i+1; j < len; j++){
+                if(arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
-                i++;
             }
-            //swap smallest element with startIndex's element
+            //swap smallest element with ith element
             temp = arr[minIndex] ;
-            arr[minIndex] = arr[startIndex];
-            arr[startIndex] = temp;
-
-            minIndex = ++startIndex;
-            i = startIndex+1;
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
             System.out.println("While sorting: " + Arrays.toString(arr));
         }
         System.out.println("After sorting: " + Arrays.toString(arr));
