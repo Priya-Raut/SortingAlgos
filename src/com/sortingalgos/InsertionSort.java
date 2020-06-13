88fd3c39dccddd888e0621532a0f05afc9e4c29e
Priya Raut <rautpriya215@gmail.com>
2020-06-13 17:24:28 +0530

    Update InsertionSort.java

--- src/com/sortingalgos/InsertionSort.java
+++ src/com/sortingalgos/InsertionSort.java
@@ -1,4 +1,23 @@
 package com.sortingalgos;
 
+import java.util.Arrays;
+
 public class InsertionSort {
+    public static int[] sortWithInsertionSort(int[] arr){
+        int oldIndex = 0, newIndex = 1, temp = 0, len = arr.length;
+        System.out.println(" ### Insertion Sort ### ");
+        System.out.println("Before sorting: " + Arrays.toString(arr));
+        while(newIndex < len){
+            temp = arr[newIndex];
+            while(oldIndex >= 0 && temp < arr[oldIndex]){
+                arr[oldIndex+1] = arr[oldIndex];
+                oldIndex--;
+            }
+            arr[oldIndex+1] = temp;
+            newIndex++; oldIndex = newIndex-1;
+            System.out.println("While sorting: " + Arrays.toString(arr));
+        }
+        System.out.println("After sorting: " + Arrays.toString(arr));
+        return arr;
+    }
 }
